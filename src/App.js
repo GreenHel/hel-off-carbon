027ce2027e025b92d1ac3c6f5bbb8b6fed0d3ede
vonpessi <juhopessi1@gmail.com>
2019-11-15 10:04:01 +0200

    header navigation and another navigation fixes

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import Container from "./components/Container";
 import Home from "./components/Home";
+import Navigation from "./components/Navigation";
 
 const App = () => (
   <Router>
     <div >
+      <Navigation name="topnav" />
       <Home />
       <Container />
     </div>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,31 +2,132 @@ import React from "react";
 import styled from "styled-components";
 import { NavLink, Link } from "react-router-dom";
 
-function Navigation() {
-  return (
-    <Wrapper>
-      <div >
-      <ul className='ul'>
-        <li>
-          <Link className='link' onClick={() => window.scrollTo({
-  top: 0,
-  behavior: 'smooth',
-})}>Home</Link>
-        </li>
-        <li>
-          <NavLink className='link' activeClassName='glow' to="/what">what</NavLink>
-        </li>
-        <li>
-          <NavLink className='link' activeClassName='glow' to="/why">why</NavLink>
-        </li>
-        <li>
-          <NavLink className='link' activeClassName='glow' to="/how">how</NavLink>
-        </li>
-      </ul>
-      </div>
-    </Wrapper>
-  );
+function ScrollPage() {
+  window.scrollTo({
+    top: 1000,
+    behavior: 'smooth',
+  })
 }
+
+function Navigation(props) {
+
+  if (props.name === "topnav") {
+
+    return (
+      <TopNavi>        
+        <div className='topnavdiv'>
+          <ul className='topnav'>
+            <li>
+              <Link className='link'  onClick={() => window.scrollTo({
+                top: 0,
+                behavior: 'smooth',
+              })}>Home</Link>
+            </li>
+            <li> 
+              <NavLink className='link' activeClassName='topglow' onClick={() => ScrollPage() } to="/what">what</NavLink>
+            </li>
+            <li>
+              <NavLink className='link' activeClassName='topglow' onClick={() => ScrollPage() } to="/why">why</NavLink>
+            </li>
+            <li>
+              <NavLink className='link' activeClassName='topglow' onClick={() => ScrollPage() } to="/how">how</NavLink>
+            </li>
+          </ul>
+        </div>
+        </TopNavi>
+
+    );
+
+
+  } else {
+
+
+    return (
+      <Wrapper>
+        <div >
+          <ul className='ul'>
+            <li>
+              <Link className='link' onClick={() => window.scrollTo({
+                top: 0,
+                behavior: 'smooth',
+              })}>Home</Link>
+            </li>
+            <li>
+              <NavLink className='link' activeClassName='glow' to="/what">what</NavLink>
+            </li>
+            <li>
+              <NavLink className='link' activeClassName='glow' to="/why">why</NavLink>
+            </li>
+            <li>
+              <NavLink className='link' activeClassName='glow' to="/how">how</NavLink>
+            </li>
+          </ul>
+        </div>
+      </Wrapper>
+    );
+  }
+}
+const TopNavi = styled.div`
+background-color: #2A2A2A;
+color: #fff;
+display: flex;
+flex-direction: row;
+
+  .topnavdiv {
+    background-color: #2A2A2A;
+  }
+
+  .topnav {
+    padding: 15px;
+    list-style: none;
+    display: flex;
+    flex-direction: row;
+    display: flex;
+    justify-content: space-between;
+    max-width: 50vh;
+
+    z-index: 1;
+    position: fixed;
+    top: 0;
+    width: 100%;
+    }  
+    .ul {
+      list-style: none;
+    }
+  
+    .link {
+      color: #5a5a5a;
+      text-decoration: none;
+      }
+
+.topglow {
+  color: #fff;
+  text-align: center;
+  -webkit-animation: glow 1s ease-in-out infinite alternate;
+  -moz-animation: glow 1s ease-in-out infinite alternate;
+  animation: glow 1s ease-in-out infinite alternate;
+}
+
+@keyframes topglow {
+  from {
+    text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #f5f3f4,
+      0 0 40px #f5f3f4, 0 0 50px #f5f3f4, 0 0 60px #f5f3f4, 0 0 70px #f5f3f4;
+  }
+  to {
+    text-shadow: 0 0 20px #fff, 0 0 30px #f5f3f4, 0 0 40px #f5f3f4,
+      0 0 50px #f5f3f4, 0 0 60px #f5f3f4, 0 0 70px #f5f3f4, 0 0 80px #f5f3f4;
+  }
+}
+}
+  span {
+    color: #fff;
+    font-size: 40px;
+  }
+
+  section {
+    color: #fff;
+  }
+`;
 
 const Wrapper = styled.div`
 background-color: #2A2A2A;
@@ -36,7 +137,25 @@ flex-direction: row;
 align-items: center;
 justify-content: center;
 text-align: center;
-padding: 70px;
+
+  .topnavdiv {
+    background-color: #2A2A2A;
+  }
+
+  .topnav {
+    padding: 15px;
+    list-style: none;
+    display: flex;
+    flex-direction: row;
+    display: flex;
+    justify-content: space-between;
+    max-width: 50vh;
+
+    z-index: 1;
+    position: fixed;
+    top: 0;
+    width: 100%;
+    }
 
   .div {
     padding: 70px;
@@ -52,6 +171,8 @@ padding: 70px;
     color: #5a5a5a;
     text-decoration: none;
     }
+
+  
 
   .glow {
     font-size: 80px;
@@ -72,6 +193,25 @@ padding: 70px;
         0 0 50px #f5f3f4, 0 0 60px #f5f3f4, 0 0 70px #f5f3f4, 0 0 80px #f5f3f4;
     }
   }
+}
+.topglow {
+  color: #fff;
+  text-align: center;
+  -webkit-animation: glow 1s ease-in-out infinite alternate;
+  -moz-animation: glow 1s ease-in-out infinite alternate;
+  animation: glow 1s ease-in-out infinite alternate;
+}
+
+@keyframes topglow {
+  from {
+    text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #f5f3f4,
+      0 0 40px #f5f3f4, 0 0 50px #f5f3f4, 0 0 60px #f5f3f4, 0 0 70px #f5f3f4;
+  }
+  to {
+    text-shadow: 0 0 20px #fff, 0 0 30px #f5f3f4, 0 0 40px #f5f3f4,
+      0 0 50px #f5f3f4, 0 0 60px #f5f3f4, 0 0 70px #f5f3f4, 0 0 80px #f5f3f4;
+  }
+}
 }
   span {
     color: #fff;
