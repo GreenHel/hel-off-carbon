b81b12b1f9006afb481dab75a41f0223abc8818b
vonpessi <juhopessi1@gmail.com>
2019-11-20 15:11:02 +0200

    page slider component added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import Home from "./components/Home";
-import Navigation from "./components/Navigation";
-import Footer from "./components/Footer";
-import GraphView from "./components/GraphView";
-import Pages from "./components/Pages"
+import PageSlider from './components/PageSlider'
 
 const App = () => (
-  <Router>
-    <div >
-      <Navigation name="topnav" />
-      <Home />
-      <GraphView />
-      <Pages name='what'/>
-      <Pages name='why'/>
-      <Pages name='how'/>
-      <Footer />
-    </div>
-  </Router>
+  <PageSlider />
 );
 
 export default App
